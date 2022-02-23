de1349c9ca28c4eb9eb2afc241d7b89dac4eaecd
Alan Czemerinski <alanczeme@gmail.com>
2022-02-23 11:53:26 -0500

    commit tree 0f7f164dcc02dd0a58cf1e4152fc01aa8e81c35d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+    string = ""
+    if array.length == 2
+        string = array.join(' and ')
+    elsif array.length >= 3
+        for i in array
+            if i == 0
+                string << array[i]
+            # elsif i < (array.length - 1)
+            #     string << (", " + array[i])
+            else
+                string << (", and " + array[i])
+            end
+        end
+    else
+        string = array.join
+    end
+    return string
 end
